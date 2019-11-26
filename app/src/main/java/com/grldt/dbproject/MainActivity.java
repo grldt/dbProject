30980c8e539b8ce6776fadf041e7c30ef1a20a60
Gerald Stephanus <geraldstephanus@gmail.com>
2019-11-26 12:05:44 +0700

    add firebase

--- app/src/main/java/com/grldt/dbproject/MainActivity.java
+++ app/src/main/java/com/grldt/dbproject/MainActivity.java
@@ -1,14 +1,57 @@
 package com.grldt.dbproject;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import static androidx.core.content.ContextCompat.startActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btAnalytics;
+    private Button btAuth;
+    private Button btFBDatabase;
+    private Button btAdmob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btAnalytics = findViewById(R.id.bt_fbanalytics);
+        btAnalytics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(MainActivity.this, FirebaseAnalyticsActivity.class));
+            }
+        });
+
+        btAuth = findViewById(R.id.bt_fbauth);
+        btAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(MainActivity.this, FirebaseAuthActivity.class));
+            }
+        });
+
+        btFBDatabase = findViewById(R.id.bt_fbdatabase);
+        btFBDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(MainActivity.this, FirebaseDBActivity.class));
+            }
+        });
+
+        btAdmob = findViewById(R.id.bt_fbadmob);
+        btAdmob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(MainActivity.this, FirebaseAdMobActivity.class));
+            }
+        });
     }
 }
